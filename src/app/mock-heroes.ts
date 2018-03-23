f72cc754faa40f64f85c9bcbe92ec5b1f0c074e4
ashokpurty <37306427+ashokpurty@users.noreply.github.com>
2018-03-24 00:00:20 +0530

    added 1st repo
    added 1st repo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[]=[
+    {id:11,name:'SKR'},
+    {id:12,name:'SALMA'},
+    {id:13,name:'AMIR'},
+    {id:14,name:'KRK'},
+    {id:15,name:'SUNNY'},
+    {id:16,name:'BEOBY'},
+    {id:17,name:'ANIL'},
+    {id:18,name:'AKSHA'},
+    {id:19,name:'ABHAY'},
+    {id:20,name:'RANBIN'},
+]
+    
